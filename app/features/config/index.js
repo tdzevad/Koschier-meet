3c9e0f5acdcb5f78ce25ec572979bc2d8a758823
tdzevad <59264041+tdzevad@users.noreply.github.com>
2020-11-04 13:30:56 +0100

    Update index.js

--- app/features/config/index.js
+++ app/features/config/index.js
@@ -3,23 +3,23 @@ export default {
     /**
      * The URL with extra information about the app / service.
      */
-    aboutURL: 'https://jitsi.org/what-is-jitsi/',
+    aboutURL: 'https://www.koschier.at/unternehmen/',
 
     /**
      * The URL to the source code repository.
      */
-    sourceURL: 'https://github.com/jitsi/jitsi-meet-electron',
+    sourceURL: 'https://github.com/tdzevad/Koschier-meet',
 
     /**
      * Application name.
      */
-    appName: 'Jitsi Meet',
+    appName: 'Koschier Meet',
 
     /**
     * The prefix for application protocol.
     * You will also need to replace this in package.json.
     */
-    appProtocolPrefix: 'jitsi-meet',
+    appProtocolPrefix: 'koschier-meet',
 
     /**
      * The default server URL of Jitsi Meet Deployment that will be used.
@@ -34,15 +34,15 @@ export default {
     /**
      * URL to send feedback.
      */
-    feedbackURL: 'mailto:support@jitsi.org',
+    feedbackURL: 'mailto:linuxadmin@koschier.it',
 
     /**
      * The URL of Privacy Policy Page.
      */
-    privacyPolicyURL: 'https://jitsi.org/meet/privacy',
+    privacyPolicyURL: 'https://www.koschier.at/datenschutzerklaerung/',
 
     /**
      * The URL of Terms and Conditions Page.
      */
-    termsAndConditionsURL: 'https://jitsi.org/meet/terms'
+    termsAndConditionsURL: 'https://www.koschier.at/datenschutzerklaerung/'
 };
